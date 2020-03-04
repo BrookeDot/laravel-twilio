48576798f203a83b0b7f1f700029439557586460
Michael Babker <michael.babker@gmail.com>
2020-03-04 11:26:20 -0600

    Note the manager's extend method in the facade doc block

--- src/Facades/TwilioClient.php
+++ src/Facades/TwilioClient.php
@@ -3,18 +3,20 @@
 namespace BabDev\Twilio\Facades;
 
 use BabDev\Twilio\Contracts\TwilioClient as TwilioClientContract;
+use BabDev\Twilio\ConnectionManager;
 use Illuminate\Support\Facades\Facade;
 use Twilio\Rest\Api\V2010\Account\CallInstance;
 use Twilio\Rest\Api\V2010\Account\MessageInstance;
 use Twilio\Rest\Client;
 
 /**
+ * @method static ConnectionManager extend($name, \Closure $callback)
  * @method static TwilioClientContract connection(string $name = null)
  * @method static Client twilio()
  * @method static CallInstance call(string $to, array $params = [])
  * @method static MessageInstance message(string $to, string $message, array $params = [])
  *
- * @see \BabDev\Twilio\ConnectionManager
+ * @see ConnectionManager
  */
 final class TwilioClient extends Facade
 {
