83fe5b6aa2b809d042f1bbcbfd028fe12726281b
Michael Babker <michael.babker@gmail.com>
2020-03-03 22:19:12 -0600

    Scaffold a connection manager

--- src/Providers/TwilioProvider.php
+++ src/Providers/TwilioProvider.php
@@ -2,10 +2,25 @@
 
 namespace BabDev\Twilio\Providers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-final class TwilioProvider extends ServiceProvider
+final class TwilioProvider extends ServiceProvider implements DeferrableProvider
 {
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'babdev.twilio.manager',
+            ConnectionManager::class,
+        ];
+    }
+
     /**
      * Bootstrap any application services.
      *
@@ -28,6 +43,15 @@ final class TwilioProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->singleton(
+            'babdev.twilio.manager',
+            static function (Application $app) {
+                return new ConnectionManager($app);
+            }
+        );
+
+        $this->app->alias('babdev.twilio.manager', ConnectionManager::class);
+
         $this->mergeConfigFrom(__DIR__ . '/../../config/twilio.php', 'twilio');
     }
 }
--- tests/Providers/TwilioProviderTest.php
+++ tests/Providers/TwilioProviderTest.php
@@ -2,6 +2,7 @@
 
 namespace BabDev\Twilio\Tests\Providers;
 
+use BabDev\Twilio\Providers\ConnectionManager;
 use BabDev\Twilio\Providers\TwilioProvider;
 use Illuminate\Support\ServiceProvider;
 use Orchestra\Testbench\TestCase;
@@ -15,6 +16,12 @@ final class TwilioProviderTest extends TestCase
             ServiceProvider::$publishes,
             'The service provider should be publishing its configuration.'
         );
+    }
+
+    public function testServicesAreRegistered(): void
+    {
+        $this->assertTrue($this->app->bound('babdev.twilio.manager'));
+        $this->assertSame('babdev.twilio.manager', $this->app->getAlias(ConnectionManager::class));
     }
 
     protected function getPackageProviders($app)
